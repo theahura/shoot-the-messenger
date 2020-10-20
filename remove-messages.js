6fe3e8de18095764b994cddccff61970135264d6
Amol Kapoor <theahura@gmail.com>
2020-10-19 22:05:55 -0700

    Create remove-messages.js
    Initial attempt

---
+++ remove-messages.js
@@ -0,0 +1,11 @@
+for (let more_button of document.querySelectorAll('._3058._ui9._hh7._6ybn._s1-._52mr._43by._6ybp._3oh- ._8sop._2rvp._7i2l')) {
+  more_button.click();
+}
+
+for (let remove_button of document.getElementsByClassName('_hw5')) {
+  remove_button.click()
+}
+
+for (let unsend_button of document.getElementsByClassName('_3quh _30yy _2t_ _3ay_ _5ixy')) {
+  unsend_button.click();
+}
