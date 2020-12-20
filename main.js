c399850b1f772eab0d6181e9ee38b55a9a45cc6b
amol kapoor <theahura@gmail.com>
2020-12-20 01:28:22 -0500

    Responding to facebook search breakage by reenabling old scrolling mechanism of message removal.

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+// The sideways ellipses used to open the 'remove' menu. To the left of each
+// message, generally visible on hover.
+MORE_BUTTONS_HOLDER_QUERY =
+  '[data-testid="outgoing_group"] [aria-label="Message actions"]';
+MORE_BUTTONS_QUERY = '[aria-label="More"]';
+
+// The button used to open the remove confirmation dialog.
+REMOVE_BUTTON_QUERY = '[aria-label="Remove message"]';
+
+// The button used to close the 'message removed' post confirmation.
+OKAY_BUTTON_QUERY = '[aria-label="Okay"]';
+
+// The button used to get rid of the Could Not Remove Message popup.
+COULDNT_REMOVE_QUERY = "._3quh._30yy._2t_._5ixy.layerCancel";
+
+// The button used to confirm the message removal.
+REMOVE_CONFIRMATION_QUERY = '[aria-label="Remove"]';
+
+// The holder for all of the messages in the chat.
+SCROLLER_QUERY =
+  '[role="main"] .buofh1pr.j83agx80.eg9m0zos.ni8dbmo4.cbu4d94t.gok29vw1';
+
+MESSAGES_QUERY = "[aria-label=Messages]";
+
+// The loading animation.
+LOADING_QUERY = '[role="main"] [aria-valuetext="Loading..."]';
+
+// The div holding the inbox (used for scrolling).
+INBOX_QUERY =
+  ".q5bimw55.rpm2j7zs.k7i0oixp.gvuykj2m.j83agx80.cbu4d94t.ni8dbmo4.eg9m0zos.l9j0dhe7.du4w35lb.ofs802cu.pohlnb88.dkue75c7.mb9wzai9.d8ncny3e.buofh1pr.g5gj957u.tgvbjcpo.l56l04vs.r57mb794.kh7kg01d.c3g1iek1.k4xni2cv";
+
+// The button used to keep scrolling up after a search in the messenger chain.
+// TODO(theahura): FB Dec 2021 update kills the search in conversation
+// feature. These queries are outdated.
+LOAD_MORE_QUERY = null;
+SEARCH_BAR_QUERY = null;
+SEARCH_CANDIDATE_QUERY = null;
+HIGHLIGHTED_QUERY = null;
+SEARCH_IN_CONVO_QUERY = null;
+NEXT_SEARCH_QUERY = null;
+PREVIOUS_SEARCH_QUERY = null;
+
 (function() {
   STATUS = {
     CONTINUE: "continue",
@@ -10,66 +52,85 @@
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
+  function getSiblings(el) {
+    // Setup siblings array and get the first sibling
+    var siblings = [];
+    var sibling = el.parentNode.firstChild;
+
+    // Loop through each sibling and push to the array
+    while (sibling) {
+      if (sibling.nodeType === 1 && sibling !== el) {
+        siblings.push(sibling);
+      }
+      sibling = sibling.nextSibling;
+    }
+
+    return siblings;
+  }
+
   // Removal functions ---------------------------------------------------------
   async function unsendAllVisibleMessages(lastRun) {
     // Click on all ... buttons that let you select 'more' for all messages you
     // sent.
-    let more_buttons = document.querySelectorAll(
-      '.clearfix._o46._3erg._3i_m._nd_.direction_ltr.text_align_ltr div:not([data-tooltip-content*="Unsent"]) [aria-label="More"]'
+    const more_buttons_holders = document.querySelectorAll(
+      MORE_BUTTONS_HOLDER_QUERY
     );
+    console.log("Found hidden menu holders: ", more_buttons_holders);
+    [...more_buttons_holders].map(el => {
+      el.click();
+    });
+
+    const more_buttons = document.querySelectorAll(MORE_BUTTONS_QUERY);
     console.log("Clicking more buttons: ", more_buttons);
-    for (let more_button of more_buttons) {
-      more_button.click();
-    }
+    [...more_buttons]
+      .filter(el => {
+        return getSiblings(el.parentNode.parentNode.parentNode).length > 1;
+      })
+      .map(el => {
+        el.click();
+      });
 
-    // Click on all of the 'remove' popups that appear. There may be other
-    // options, like 'Save to Facebook' -- make sure to drop those.
-    let remove_buttons = document.getElementsByClassName("_hw5");
+    // Click on all of the 'remove' popups that appear.
+    const remove_buttons = document.querySelectorAll(REMOVE_BUTTON_QUERY);
     console.log("Clicking remove buttons: ", remove_buttons);
-    for (let remove_button of remove_buttons) {
-      if (remove_button.textContent !== "Remove") continue;
-      remove_button.click();
-    }
-    await sleep(2000);
+    [...remove_buttons].map(el => {
+      el.click();
+    });
 
-    // Each one of those remove buttons will pull up a modal for confirmation.
-    // Click all of those modals too. If a minute has passed and we're still
-    // pushing modals, keep going; something probably got stuck.
-    let unsend_buttons = document.getElementsByClassName(
-      "_3quh _30yy _2t_ _3ay_ _5ixy"
-    );
-    for (let i = 0; unsend_buttons.length > 0 || i < 10; ++i) {
-      console.log("Clicking unsend buttons: ", unsend_buttons);
-      for (let unsend_button of unsend_buttons) {
-        unsend_button.click();
-        await sleep(5000);
-      }
-      unsend_buttons = document.getElementsByClassName(
-        "_3quh _30yy _2t_ _3ay_ _5ixy"
-      );
+    // Click on all of the 'confirm remove' buttons.
+    const unsend_buttons = document.querySelectorAll(REMOVE_CONFIRMATION_QUERY);
+    console.log("Unsending: ", unsend_buttons);
+    for (let unsend_button of unsend_buttons) {
+      unsend_button.click();
+      await sleep(2000);
     }
 
-    // Sometimes a remove fails for inexplicable reasons, likely rate limting.
-    // Loop until those are gone.
-    let couldntremoves = document.getElementsByClassName(
-      "_3quh _30yy _2t_ _5ixy layerCancel"
-    );
-    while (couldntremoves.length > 0) {
-      console.log("Got couldnt remove dialogs: ", couldntremoves);
-      console.log("Waiting 5 minutes.");
+    // for (let remove_button of remove_buttons) {
+    //   // Sometimes a remove fails for inexplicable reasons, likely rate limting.
+    //   // To handle this, we loop until we confirm the message is deleted.
+    //   while (true) {
+    //     remove_button.click();
+    //     await sleep(2000);
+    //     // Each one of those remove buttons will pull up a modal for confirmation.
+    //     // Click the modal too.
+    //     console.log("Unsending: ", unsend_button);
+    //     unsend_button.click();
+    //     await sleep(2000);
 
-      // If we got couldn't removes, its because of rate limiting. Wait 5
-      // minutes before starting up again to see if that clears the issue.
-      await sleep(300000);
+    //     // There might be a post confirmation message letting you know about the
+    //     // removal. Get rid of it.
+    //     const maybe_ok_button = document.querySelector(OKAY_BUTTON_QUERY);
+    //     if (maybe_ok_button) maybe_ok_button.click();
+    //     await sleep(2000);
 
-      for (let couldntremove of couldntremoves) {
-        couldntremove.click();
-        await sleep(200);
-      }
-      couldntremoves = document.getElementsByClassName(
-        "_3quh _30yy _2t_ _5ixy layerCancel"
-      );
-    }
+    //     let couldntremove = document.querySelector(COULDNT_REMOVE_QUERY);
+    //     if (couldntremove) {
+    //       await 3000000;
+    //     } else {
+    //       break;
+    //     }
+    //   }
+    // }
 
     // If this is the last run before the runner cycle finishes, dont keep
     // scrolling up.
@@ -78,11 +139,10 @@
     }
 
     // Cleaned out all the couldnt remove buttons. Now, check to see if we need
-    // to hit the 'Load More' button or if we just need to scroll up.
-    const maybeLoaders = document.getElementsByClassName(
-      "_3quh _30yy _2t_ _41jf"
-    );
-    const scroller_ = document.querySelector("._4u-c._1wfr._9hq [id*=js_]");
+    // to hit the 'Load More' button or if we need to scroll up.
+    const maybeLoaders = document.querySelectorAll(LOAD_MORE_QUERY);
+    const scroller_ = document.querySelector(SCROLLER_QUERY);
+    await sleep(2000);
     if (maybeLoaders.length > 1) {
       // We should have two load more buttons, unless we've hit the top.
       console.log("Clicking load more.");
@@ -96,10 +156,27 @@
       } catch (err) {
         console.log(err);
       }
+
+      // Remove all of the previously covered messages from the DOM to try and
+      // save memory.
+      // let messages = document.querySelector(MESSAGES_QUERY);
+      // console.log("Removing elements from: ", messages);
+      // while (messages.childNodes.length > 150) {
+      //   messages.removeChild(messages.lastChild);
+      // }
+
+      // Don't continue until the loading animation is gone.
+      await sleep(2000);
+      let loader = document.querySelector(LOADING_QUERY);
+      while (loader) {
+        console.log("Waiting for loading messages to populate...", loader);
+        await sleep(2000);
+        loader = document.querySelector(LOADING_QUERY);
+      }
     } else {
       // There's no Load More button and there's no more scrolling up, so we
       // probably sucessfully finished. Bubble this info back up.
-      console.log("Reached top of chain.");
+      console.log("Reached top of chain: ", scroller_.scrollTop);
       return { status: STATUS.COMPLETE };
     }
 
@@ -134,9 +211,9 @@
     console.log("Set up search bar. Starting removal process.");
     let searchInConvo = null;
     for (let i = 0; !searchInConvo || i < 10; ++i) {
-      searchInConvo = [...document.getElementsByClassName("_3szq")].filter(
-        el => el.innerHTML === "Search in Conversation"
-      )[0];
+      searchInConvo = [
+        ...document.querySelectorAll(SEARCH_IN_CONVO_QUERY)
+      ].filter(el => el.innerHTML === "Search in Conversation")[0];
 
       if (!searchInConvo) await sleep(5000);
     }
@@ -150,10 +227,8 @@
 
     console.log("Got searchInConvo: ", searchInConvo);
 
-    let searchBar = document.querySelector(
-      '*[placeholder="Search in Conversation"]'
-    );
-    let previousSearch = document.querySelector("._33p7[role=presentation]");
+    let searchBar = document.querySelector(SEARCH_BAR_QUERY);
+    let previousSearch = document.querySelector(PREVIOUS_SEARCH_QUERY);
 
     if (searchBar || previousSearch) {
       // Need to reboot the search bar.
@@ -174,9 +249,7 @@
     }
 
     // Either way, need to re-query the search bar because we recreated it.
-    searchBar = document.querySelector(
-      '*[placeholder="Search in Conversation"]'
-    );
+    searchBar = document.querySelector(SEARCH_BAR_QUERY);
     searchBar.focus();
     searchBar.value = searchText;
 
@@ -200,16 +273,14 @@
     // As a heuristic, we stop at the first message where every highlighted
     // word also appears in the searchText. This has obvious failure modes,
     // but is also probably sufficient for natural language.
-    const nextButton = document.getElementsByClassName(
-      "_3quh _30yy _2t_ _-19 _b-u"
-    )[0];
+    const nextButton = document.querySelectorAll(NEXT_SEARCH_QUERY)[0];
     const expectedMatcherLength = searchText
       .split(/\s+/)
       .filter(word => word.length > 3).length;
     console.log("Looking for message with N matches: ", expectedMatcherLength);
     for (let i = 0; i < 20; ++i) {
       await sleep(5000);
-      const highlighted = document.getElementsByClassName("__in");
+      const highlighted = document.querySelectorAll(HIGHLIGHTED_QUERY);
       console.log("Highlighted: ", highlighted);
       if (highlighted.length === 0) {
         // Hit a weird case where the search button wasn't actually pressed.
@@ -236,7 +307,7 @@
     // Get all the candidate search messages. Cut each one down to a 15 word
     // string. Remove any that are the same as the current searchText or have
     // fewer than 5 words with length greater than 3.
-    const candidates = [...document.getElementsByClassName("_3oh- _58nk")];
+    const candidates = [...document.querySelectorAll(SEARCH_CANDIDATE_QUERY)];
     console.log("Candidates: ", candidates);
     const processedCandidates = candidates
       .map(el =>
@@ -285,40 +356,40 @@
       }
     }
 
-    const actualRunnerCount = runnerCount ? runnerCount : 10;
     for (let i = 0; i < count || !count; ++i) {
       console.log("On run: ", i);
-      const status = await runner(actualRunnerCount);
+      const status = await runner(runnerCount);
       console.log("Runner status: ", status);
       if (status === STATUS.COMPLETE) return { status: status };
 
-      const maybeSearchText = await getNextSearchText(searchText);
-      if (maybeSearchText.status === STATUS.CONTINUE) {
-        console.log("Next search is: ", maybeSearchText.data);
-        searchText = maybeSearchText.data;
-      } else {
-        console.log(
-          "Encountered error. All messages may not have been deleted."
-        );
+      // TODO(theahura): FB Dec 2021 update kills the search in conversation
+      // feature. These queries are outdated.
+      //
+      // const maybeSearchText = await getNextSearchText(searchText);
+      // if (maybeSearchText.status === STATUS.CONTINUE) {
+      //   console.log("Next search is: ", maybeSearchText.data);
+      //   searchText = maybeSearchText.data;
+      // } else {
+      //   console.log(
+      //     "Encountered error. All messages may not have been deleted."
+      //   );
 
-        // Check one last time to make sure this was in fact an error and not a
-        // reason to clear.
-        console.log("Testing error status.");
-        const maybeStatus = await runner(actualRunnerCount);
-        if (maybeStatus === STATUS.COMPLETE) return { status: maybeStatus };
-        console.log("Confirmed error.");
+      //   // Check one last time to make sure this was in fact an error and not a
+      //   // reason to clear.
+      //   console.log("Testing error status.");
+      //   const maybeStatus = await runner(actualRunnerCount);
+      //   if (maybeStatus === STATUS.COMPLETE) return { status: maybeStatus };
+      //   console.log("Confirmed error.");
 
-        return { status: STATUS.ERROR };
-      }
+      //   return { status: STATUS.ERROR };
+      // }
     }
     return { status: STATUS.CONTINUE, data: searchText };
   }
 
   // Scroller functions --------------------------------------------------------
   function scrollToBottomHelper() {
-    let scroller = document.querySelectorAll(
-      "._5f0v .uiScrollableAreaWrap.scrollable"
-    )[0];
+    let scroller = document.querySelectorAll(INBOX_QUERY)[0];
     scroller.scrollTop = scroller.scrollHeight;
   }
 
@@ -329,6 +400,7 @@
     }
   }
 
+  // Handlers ------------------------------------------------------------------
   const currentURL =
     location.protocol + "//" + location.host + location.pathname;
 
@@ -336,7 +408,10 @@
     const prevSearchText = msg.prevSearchText
       ? msg.prevSearchText["nextSearchText"]
       : null;
-    const maybeSearchText = await longChain(10, 10, prevSearchText);
+
+    // TODO(theahura): FB Dec 2021 update kills the search in conversation
+    // feature. These queries are outdated.
+    const maybeSearchText = await longChain(0, 0, prevSearchText);
     if (maybeSearchText.status === STATUS.COMPLETE) {
       console.log(
         "Possibly successfully removed all messages. Running one more confirmation attempt."
